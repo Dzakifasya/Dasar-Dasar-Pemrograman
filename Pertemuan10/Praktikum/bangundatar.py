e0d36f95e00650cfb6e6f563e94a61314e27a44c
Dzaki Fasya <dzakifasya4@gmail.com>
2025-11-28 15:23:02 +0700

    update

---
+++ Pertemuan10/Praktikum/bangundatar.py
@@ -0,0 +1,19 @@
+import math
+def persegi(sisi):
+    hasil = sisi * sisi
+    return hasil
+
+def persegipanjang(panjang, lebar):
+    hasil = panjang * lebar
+    return hasil
+
+def lingkaran(jari_jari):
+    hasil = math.pi * math.pow(jari_jari, 2)
+    return hasil
+
+def segitiga(alas, tinggi):
+    return(alas * tinggi) / 2
+
+def jajargenjang(alas, tinggi):
+    hasil = alas * tinggi
+    return hasil
---
+++ Pertemuan10/Praktikum/call.py
@@ -0,0 +1,7 @@
+import bangundatar as bd
+
+print("Menghitung Luas Persegi dengan sisi %i cm adalah %i cm" % (4, bd.persegi(4)))
+print("Menghitung Luas Persegi Panjang dengan panjang %i cm dan lebar %i cm adalah %i cm" % (5, 3, bd.persegipanjang(5, 3)))
+print("Menghitung Luas Lingkaran dengan jari-jari %i cm adalah %.2f cm" % (7, bd.lingkaran(7)))
+print("Menghitung Luas Segitiga dengan alas %i cm dan tinggi %i cm adalah %.2f cm" % (6, 4, bd.segitiga(6, 4)))
+print("Menghitung Luas Jajar Genjang dengan alas %i cm dan tinggi %i cm adalah %i cm" % (8, 5, bd.jajargenjang(8, 5)))
